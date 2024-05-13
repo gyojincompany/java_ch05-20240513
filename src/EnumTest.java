877bbcd750f8981103eaa4f0f1ce11613c5cf4aa
gyojincompany <luckyred@gmail.com>
2024-05-13 15:48:16 +0900

    배열, 열거형 연습

---
+++ src/EnumTest.java
@@ -0,0 +1,11 @@
+
+public class EnumTest {
+	public enum Week {MONDAY, TUESDAY, WEDNESDAY, THURSDAY, FRIDAY, SATURDAY, SUNDAY}
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Week today;
+		today = Week.SUNDAY;
+	}
+
+}
